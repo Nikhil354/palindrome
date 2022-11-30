6d89bd52674102083cee6939d07116db8106a258
Manish Raj <nikhilkr354@gmail.com>
2022-11-30 14:43:17 +0530

    checkpalindrome

---
+++ expalindrome.js
@@ -0,0 +1,13 @@
+function checkPalindrome(N, str) {
+    //write code here
+    let bag='';
+    for(let i=N-1; i>=0; i--){
+        bag +=str[i]+'';
+    }
+    if(bag===str){
+        console.log('Yes');
+    }else{
+        console.log('No');
+    }
+    
+}
